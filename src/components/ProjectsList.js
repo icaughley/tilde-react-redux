32fffe11a3e91c5f42fcb56dfff62c003db5baf1
Ian Caughley <Ian@StockSoftware.com.au>
2017-04-26 13:48:44 +0800

    Added a projects list.

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,18 +1,42 @@
 import React from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import "../style/style.css";
 
 export default function ProjectsList({projects}) {
     const emptyMessage = (
         <p>There no projects yet.</p>
     );
 
-    const projectsList = (
-        <p>List of projects</p>
+    const rows = projects.map(project => {
+        return (
+            <tr key={project.id}>
+                <td>{project.id}</td>
+                <td>{project.name}</td>
+            </tr>
+        )
+    });
+
+    const projectsTable = (
+        <table className="view-projects">
+            <thead>
+            <tr>
+                <td className="table-heading" colspan="2">Project Cloaking</td>
+            </tr>
+
+            <tr>
+                <th>Project Id</th>
+                <th>Name</th>
+            </tr>
+            </thead>
+            <tbody>
+            {rows}
+            </tbody>
+        </table>
     );
 
     return (
         <div>
-            {projects.length === 0 ? emptyMessage : projectsList}
+            {projects.length === 0 ? emptyMessage : projectsTable}
         </div>
     );
 }
