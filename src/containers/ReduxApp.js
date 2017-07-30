2b531393fa3cb9dd82d22484d3a68070b9438339
Ian Caughley <Ian@StockSoftware.com.au>
2017-07-30 12:56:42 +1000

    Added token based security for REST calls.

--- src/containers/ReduxApp.js
+++ src/containers/ReduxApp.js
@@ -8,10 +8,22 @@ import {composeWithDevTools} from "redux-devtools-extension";
 import {Provider} from "react-redux";
 import thunk from "redux-thunk";
 import ReduxPromise from "redux-promise";
+import axios from "axios";
 
 const store = createStore(
     reducers,
     composeWithDevTools(applyMiddleware(ReduxPromise, thunk, authentication, messages))
+);
+
+axios.interceptors.request.use(config => {
+        config.headers = {
+            ...config.headers,
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: store.getState().auth ? store.getState().auth.token : ""
+        };
+        return config;
+    }
 );
 
 export default () => (
